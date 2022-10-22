78608bc8ee24f0401eadf4b56e7bcc78db61532e
Madhur <madhur12a16072@gmail.com>
2022-10-23 01:58:20 +0530

    Reuploaded

---
+++ app/src/main/java/com/example/signin/MainActivity2.kt
@@ -0,0 +1,13 @@
+package com.example.signup
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.signin.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
